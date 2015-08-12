c2bee35ac193f1ff511616d4c9e8573cb3bc6013
Catherinecao <catherinecao25@gmail.com>
2015-08-12 12:30:13 +1200

    wireframe 0.1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
+	var $schoolBackground = $(".school-background"),
+		$navBar = $(".navbar"),
+		$navBarHeight = $navBar.height();
 
+
+	//$schoolBackground.css("margin-top": $navBarHeight);
 });
